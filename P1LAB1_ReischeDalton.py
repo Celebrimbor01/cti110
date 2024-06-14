d5cb142d96f0a1a5072e9c47b120a815962f11e9
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2024-06-14 19:48:22 -0400

    Add files via upload
    First submission

---
+++ P1LAB1_ReischeDalton.py
@@ -0,0 +1,10 @@
+ # Dalton Reische
+ # 6/11/2024
+ # P1LAB1
+ # Using print and input functions to display a line of text
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input() + '!'
+print('Hello,', first_name, last_name, 'Welcome to CTI-110')
+
