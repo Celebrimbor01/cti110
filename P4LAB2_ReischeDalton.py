d42597bc0821af22227c81e7b26c9ee2e551bab7
Celebrimbor01 <reisched1830@student.faytechcc.edu>
2024-07-03 15:49:00 -0400

    Add files via upload
    Using while and for loops to multiply a user input

---
+++ P4LAB2_ReischeDalton.py
@@ -0,0 +1,24 @@
+ # Dalton Reische
+ # 7/3/2024
+ # P4LAB2
+ # use while and for loop to multiply a user input
+
+#Loop
+ 
+while True:
+    num = int(input("Enter an integer: "))
+    print()
+        
+    if num < 0:
+        print("This program does not handle negative numbers.")
+    else:
+        for i in range(1, 13):
+             print(f"{num} * {i} = {num * i}")
+    print()
+        
+    repeat = input("Would you like to run this program again? ")
+    if repeat != "yes":
+        break
+    print()
+    
+
